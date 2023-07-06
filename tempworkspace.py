0c8ac2e858e86dedc08311affa46f796dbf6df81
Jacob Verburg <verburg.jacob@gmail.com>
2023-07-06 13:39:29 -0700

    Finished section 7, new computer so had to re set up project a bit.

--- tempworkspace.py
+++ tempworkspace.py
@@ -1,14 +1,35 @@
-# 1) Import the random function and generate both a random number between 0 and 1 as well as a random number between 1 and 10.
-import datetime
-import random
+#1) Write a short Python script which queries the user for input (infinite loop with exit possibility) and writes the input to a file.
+import pickle
 
-random_number_zero_to_1 = random.random()
-print(str(random_number_zero_to_1))
-random_number_1_to_10 = random.randint(1, 10)
-print(str(random_number_1_to_10))
+#2) Add another option to your user interface: The user should be able to output the data stored in the file in the terminal.
 
-# 2) Use the datetime library together with the random number to generate a random, unique value.
+#3) Store user input in a list (instead of directly adding it to the file) and write that list to the file – both with pickle and json.
 
-unique_value = str(datetime.datetime.now()) + str(random_number_1_to_10)
+#4) Adjust the logic to load the file content to work with pickled/ json data.
 
-print(unique_value)
+
+
+waiting_for_input = True
+user_input_list = []
+
+while waiting_for_input:
+    print('Please choose')
+    print('1: Enter text and save')
+    print('2: load and output')
+    print('q: Quit')
+    user_choice = input('Your Choice: ')
+    if user_choice == '1':
+        tx_data = input('Your Text: ')
+        user_input_list.append(tx_data)
+        with open('daFile.txt', mode='wb') as f:
+            f.write(pickle.dumps(user_input_list))
+    elif user_choice == '2':
+        with open('daFile.txt', mode='rb') as f:
+            file_content = pickle.loads(f.read())
+            for line in file_content:
+                print(line)
+    elif user_choice == 'q':
+        # This will lead to the loop to exist because it's running condition becomes False
+        waiting_for_input = False
+    else:
+        print('Input was invalid, please pick a value from the list!')
